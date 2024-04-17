6a99fa4d36a9309642d398d2f46a49158824062f
Krishna <hanumaramavath9010@gmail.com>
2024-04-17 16:26:36 +0530

    Configuration Management

--- v2-spring-cloud-config/cards/src/main/java/com/microservices/cards/dto/CardsContactInfoDto.java
+++ v2-spring-cloud-config/cards/src/main/java/com/microservices/cards/dto/CardsContactInfoDto.java
@@ -4,7 +4,16 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import lombok.Getter;
+import lombok.Setter;
 @ConfigurationProperties(prefix = "cards")
-public record CardsContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+@Getter
+@Setter
+public class CardsContactInfoDto {
+
+	private String message;
+	private Map<String, String> contactDetails;
+	private List<String> onCallSupport;
 
 }
