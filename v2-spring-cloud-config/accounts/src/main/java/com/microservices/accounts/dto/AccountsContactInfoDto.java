f3f105e54aabd07448d04614e68a1a45508e1434
Krishna <hanumaramavath9010@gmail.com>
2024-04-18 16:55:11 +0530

    External Configuration

--- v2-spring-cloud-config/accounts/src/main/java/com/microservices/accounts/dto/AccountsContactInfoDto.java
+++ v2-spring-cloud-config/accounts/src/main/java/com/microservices/accounts/dto/AccountsContactInfoDto.java
@@ -9,11 +9,28 @@ import lombok.Getter;
 import lombok.Setter;
 
 @ConfigurationProperties(prefix = "accounts")
-@Getter
-@Setter
+
 public class AccountsContactInfoDto {
 	private String message;
 	private Map<String, String> contactDetails;
 	private List<String> onCallSupport;
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public Map<String, String> getContactDetails() {
+		return contactDetails;
+	}
+	public void setContactDetails(Map<String, String> contactDetails) {
+		this.contactDetails = contactDetails;
+	}
+	public List<String> getOnCallSupport() {
+		return onCallSupport;
+	}
+	public void setOnCallSupport(List<String> onCallSupport) {
+		this.onCallSupport = onCallSupport;
+	}
 
 }
--- v2-spring-cloud-config/cards/src/main/java/com/microservices/cards/dto/CardsContactInfoDto.java
+++ v2-spring-cloud-config/cards/src/main/java/com/microservices/cards/dto/CardsContactInfoDto.java
@@ -8,12 +8,29 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import lombok.Getter;
 import lombok.Setter;
 @ConfigurationProperties(prefix = "cards")
-@Getter
-@Setter
+
 public class CardsContactInfoDto {
 
 	private String message;
 	private Map<String, String> contactDetails;
 	private List<String> onCallSupport;
-
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public Map<String, String> getContactDetails() {
+		return contactDetails;
+	}
+	public void setContactDetails(Map<String, String> contactDetails) {
+		this.contactDetails = contactDetails;
+	}
+	public List<String> getOnCallSupport() {
+		return onCallSupport;
+	}
+	public void setOnCallSupport(List<String> onCallSupport) {
+		this.onCallSupport = onCallSupport;
+	}
+	
 }
